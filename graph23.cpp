e5fb18891d18103468fddf7d3acc18694274949d
RIYA MONDAL <142240539+Riya78278@users.noreply.github.com>
2026-01-07 19:48:06 +0530

    Add files via upload

---
+++ graph23.cpp
@@ -0,0 +1,32 @@
+// bfs
+#include<iostream>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> bfs(vector<vector<int>> &adj) {
+        // code here
+        int n=adj.size();
+        vector<int>vis(n,0);
+        vector<int>bfs;
+        queue<int>q;
+        
+        vis[0]=1;
+        q.push(0);
+        
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            bfs.push_back(node);
+            
+            for(auto it:adj[node]){
+                if(!vis[it]){
+                    vis[it]=1;
+                    q.push(it);
+                }
+            }
+        }
+        return bfs;
+        
+    }
+};
